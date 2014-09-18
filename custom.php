762014c23d6fc5cbec26a23cc4ad12db2c1103d9
Greg Tunink <techgique@gmail.com>
2014-09-18 04:14:14 -0500

    Add org title div and title display in narrow devices in header if dojo site; Update styles
    custom.php
    Remove old GitHub CSS include
    Re-style code of Mike's video markup function for easier reading
    
    Subdomain regex will match if anything more than a bare domain hostname
    Removes default title markup function and replaces with one that adds extra div with organization title
    
    
    custom.css
    Set organization title as light blue color and line height for spacing from other text
    
    Set min-height on title area so logo does not touch top edge of viewport
    Space from top of viewport
    
    Return site title color to white, orange on hover
    Set line height on site title for nicer spacing from tagline
    
    Set description/tagline to light blue color and make text slightly larger
    Space from menu items
    
    Remove text color changes no longer desired and unused .agency-pro-red rules
    
    Hide organization title when not a dojo site
    
    On narrow devices display site title when a dojo site; Small but bold text spaced from menu items
    
    On narrow devices allow title area to collapse with min-height: 0 rule; decrease spacing from top of viewport

--- custom.php
+++ custom.php
@@ -1,6 +1,5 @@
+<?php
 // Mike Lyon hacks Brigette Callahan SKA Shotokan Karate hacks begin here - copy these lines when upgrading theme!
-
-wp_enqueue_style('custom', 'https://raw.githubusercontent.com/ska-org/agency-pro/master/custom.css');
 
 add_image_size( '100-page', 1040, 2000, FALSE ); // Mike lyon hack SKA Brigette
 
@@ -15,13 +14,13 @@ if ( ! isset( $content_width ) ) $content_width = 1045;
 //* Mike Lyon hack to fix youtube and flash media so it doesn't overlay menus etc
 
 function add_video_wmode_transparent($html, $url, $attr) {
-
-if ( strpos( $html, "<embed src=" ) !== false )
-   { return str_replace('</param><embed', '</param><param name="wmode" value="opaque"></param><embed wmode="opaque" ', $html); }
-elseif ( strpos ( $html, 'feature=oembed' ) !== false )
-   { return str_replace( 'feature=oembed', 'feature=oembed&wmode=opaque', $html ); }
-else
-   { return $html; }
+    if ( strpos( $html, "<embed src=" ) !== false ) {
+        return str_replace('</param><embed', '</param><param name="wmode" value="opaque"></param><embed wmode="opaque" ', $html);
+    }
+    elseif ( strpos ( $html, 'feature=oembed' ) !== false ) {
+        return str_replace( 'feature=oembed', 'feature=oembed&wmode=opaque', $html );
+    }
+    else { return $html; }
 }
 add_filter( 'embed_oembed_html', 'add_video_wmode_transparent', 10, 3);
 
@@ -35,3 +34,9 @@ remove_action( 'genesis_after_entry', 'genesis_get_comments_template' );
 
 // end of Mike Lyon hacks
 
+
+if (preg_match('/[^.]+\.[^.]+\.[^.]+$/', $_SERVER['SERVER_NAME'])) {
+    remove_action('genesis_site_title', 'genesis_seo_site_title');
+    function dojo_title() {?><div class="org-title">Shotokan Karate of America</div><h1 class="site-title dojo" itemprop="headline"><a href="/"><?php bloginfo('name') ?></a></h1><?php };
+    add_action('genesis_site_title', 'dojo_title');
+}
