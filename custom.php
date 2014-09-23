79f751afbe9fbecb525f762e5f193fc764b811ec
Greg Tunink <techgique@gmail.com>
2014-09-23 01:36:57 -0500

    Display YouTube embed more appropriately for device
    custom.css
    Set the size and position of the iframe differently for each device width
    
    custom.php
    Remove content_width setting which was always setting iframe width at 1045px
    Clean up comments and organize each bit of code similar to where it affects each page
    Header  modifications first, content modifications in the middle, and comment removal last

--- custom.php
+++ custom.php
@@ -1,18 +1,22 @@
 <?php
-// Mike Lyon hacks Brigette Callahan SKA Shotokan Karate hacks begin here - copy these lines when upgrading theme!
+// Add extra SKA title element and .dojo class if a dojo site
+if (preg_match('/[^.]+\.[^.]+\.[^.]+$/', $_SERVER['SERVER_NAME'])) {
+    remove_action('genesis_site_title', 'genesis_seo_site_title');
+    function dojo_title() {?><div class="org-title">Shotokan Karate of America</div><h1 class="site-title dojo" itemprop="headline"><a href="/"><?php bloginfo('name') ?></a></h1><?php };
+    add_action('genesis_site_title', 'dojo_title');
+}
 
-add_image_size( '100-page', 1040, 2000, FALSE ); // Mike lyon hack SKA Brigette
 
-add_image_size( '050-page', 520,520, FALSE ); // Mike lyon hack SKA Brigette
+// Reinsert site description which was removed elsewhere
+add_action( 'genesis_site_description', 'genesis_seo_site_description' );
 
 
+// Set new sizes at which WordPress will save an image when uploaded
+add_image_size( '100-page', 1040, 2000, FALSE );
+add_image_size( '050-page', 520, 520, FALSE );
 
-//* Mike Lyon hack to fix default media witdh so youtube fills page
 
-if ( ! isset( $content_width ) ) $content_width = 1045;
-
-//* Mike Lyon hack to fix youtube and flash media so it doesn't overlay menus etc
-
+// Modify embeds to use wmode=opaque so they do not overlap menus
 function add_video_wmode_transparent($html, $url, $attr) {
     if ( strpos( $html, "<embed src=" ) !== false ) {
         return str_replace('</param><embed', '</param><param name="wmode" value="opaque"></param><embed wmode="opaque" ', $html);
@@ -24,19 +28,7 @@ function add_video_wmode_transparent($html, $url, $attr) {
 }
 add_filter( 'embed_oembed_html', 'add_video_wmode_transparent', 10, 3);
 
-//* add back the site description which was removed above
-add_action( 'genesis_site_description', 'genesis_seo_site_description' );
 
-//* Remove comments Mike Lyon hack
-
-remove_action( 'genesis_after_post', 'genesis_get_comments_template' );
+// Remove comments
 remove_action( 'genesis_after_entry', 'genesis_get_comments_template' );
-
-// end of Mike Lyon hacks
-
-
-if (preg_match('/[^.]+\.[^.]+\.[^.]+$/', $_SERVER['SERVER_NAME'])) {
-    remove_action('genesis_site_title', 'genesis_seo_site_title');
-    function dojo_title() {?><div class="org-title">Shotokan Karate of America</div><h1 class="site-title dojo" itemprop="headline"><a href="/"><?php bloginfo('name') ?></a></h1><?php };
-    add_action('genesis_site_title', 'dojo_title');
-}
+remove_action( 'genesis_after_post', 'genesis_get_comments_template' );
